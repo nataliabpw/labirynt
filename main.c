4ac1e48e6a8660ffa7af944daadd827f9b8a5ea8
anton <ntnlbk@gmail.com>
2024-04-01 13:10:21 +0200

    empty readmaze.h, readmaze.c added

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "maze.h"
+#include "readmaze.h"
 int main( int argc, char **argv){
 	maze maze_str = {NULL, 0, 0, 0, 0, 0};
+	
 	return 0;
 }
---
+++ readmaze.c
@@ -0,0 +1,13 @@
+#include "readmaze.h"
+
+int checkinput(char* filename){
+	return 0;
+}
+
+void read(maze* m){
+
+}
+
+int checkpassage(int node_number, int direction, maze* m){
+	return 0;
+}
